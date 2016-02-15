224d0d497c894ea148b61a1ee790c37ae56e3c0a
Christopher Beckham <chrispy645@gmail.com>
2016-02-15 12:47:34 -0500

    Add RNN exps

---
+++ vocal_synthesis/rnn_experiment.py
@@ -0,0 +1,116 @@
+import sys
+import os
+from scipy.io import wavfile
+# ---
+import numpy as np
+import theano
+from theano import tensor as T
+# ---
+import lasagne
+from lasagne.layers import *
+from lasagne.init import *
+from lasagne.nonlinearities import *
+from lasagne.objectives import *
+from lasagne.updates import *
+
+def prepare(args):
+
+    X_train = args["X_train"]
+
+    num_inputs = args["num_inputs"] # should always be 1
+    seq_length = X_train.shape[1] # determined by pkl
+    num_hidden_units = args["num_hidden_units"]
+
+    l_input = InputLayer((None, seq_length, num_inputs))
+    l_forward = RecurrentLayer(l_input, num_units=num_hidden_units)
+    """
+    In order to connect a recurrent layer to a dense layer, we need to
+    flatten the first two dimensions (our "sample dimensions"); this will
+    cause each time step of each sequence to be processed independently
+    """
+    l_shp = ReshapeLayer(l_forward, (-1, num_hidden_units))
+    l_dense = DenseLayer(l_shp, num_units=1, nonlinearity=linear)
+    l_out = ReshapeLayer(l_dense, (-1, seq_length, 1))
+    sys.stderr.write("Number of params in model: %i\n" % count_params(l_out))
+
+    X = T.tensor3('X')
+    net_out = get_output(l_out, X)
+    get_out = theano.function([X], net_out)
+
+    loss = squared_error(net_out[:,0:seq_length-1,:], X[:,1::,:]).mean()
+    params = get_all_params(l_out, trainable=True)
+    loss_fn = theano.function([X], loss)
+    updates = adagrad(loss, params, 0.01)
+
+    learning_rate = args["learning_rate"]
+    momentum = args["momentum"]
+    updates = lasagne.updates.nesterov_momentum(
+        loss, params, learning_rate=learning_rate, momentum=momentum)
+
+    train_fn = theano.function([X], loss, updates=updates)
+    eval_fn = theano.function([X], loss)
+
+    return {
+        "l_out": l_out,
+        "X": X,
+        "get_out": get_out,
+        "params": params,
+        "loss": loss,
+        "learning_rate": learning_rate,
+        "momentum": momentum,
+        "updates": updates,
+        "train_fn": train_fn,
+        "eval_fn": eval_fn
+    }
+
+def train(args):
+
+    X_train, X_valid = args["X_train"], args["X_valid"]
+
+    symbols = prepare(args)
+
+    train_fn, eval_fn = symbols["train_fn"], symbols["eval_fn"]
+    l_out = symbols["l_out"]
+
+    num_epochs = args["num_epochs"]
+    batch_size = args["batch_size"]
+
+    print "epoch,train_loss,valid_loss,has_train_loss_improved,has_valid_loss_improved"
+    
+    best_val_score_so_far = float('inf')
+    best_train_score_so_far = float('inf')
+    best_model = None
+
+    for epoch in range(0, num_epochs):
+        b = 0
+        train_losses = []
+        while True:
+            X_train_batch = X_train[b*batch_size : (b+1)*batch_size]
+            train_losses.append( train_fn(X_train_batch) )
+            b += 1
+            if b*batch_size >= len(X_train):
+                break
+            
+        this_valid_loss = eval_fn(X_valid)
+        this_valid_has_improved = 0
+        if this_valid_loss < best_val_score_so_far:
+            best_val_score_so_far = this_valid_loss
+            this_valid_has_improved = 1
+            best_model = lasagne.layers.get_all_param_values(l_out)
+            
+        this_train_loss = np.mean(train_losses)
+        this_train_has_improved = 0
+        if this_train_loss < best_train_score_so_far:
+            best_train_score_so_far = this_train_loss
+            this_train_has_improved = 1
+
+        print str(epoch) + "," \
+            + str(this_train_loss) + "," \
+            + str(this_valid_loss) + "," \
+            + str(this_train_has_improved) + "," \
+            + str(this_valid_has_improved)
+
+    return best_model
+
+def test(args):
+    pass
--- vocal_synthesis/run_exp.py
+++ vocal_synthesis/run_exp.py
@@ -1,67 +1,44 @@
-import experiment
+import rnn_experiment as experiment
 import numpy as np
 from scipy.io import wavfile
 import os
 import sys
 import cPickle as pickle
 
-def generate_default(data, chunk_size):
-    X_train = []
-    y_train = []
-    b = 0
-    while True:
-        this_chunk = data[b*chunk_size : (b+1)*chunk_size]
-        if len(this_chunk) != chunk_size:
-            break
-        X_train.append( this_chunk[0 : len(this_chunk)-1] )
-        y_train.append( [ this_chunk[len(this_chunk)-1] ] )
-        b += 1
-        if (b)*chunk_size >= len(data):
-            break
-    return np.asarray(X_train, dtype="float32"), np.asarray(y_train, dtype="float32")
+data_folder = os.environ["DATA_DIR"]
 
-data_folder = "/Volumes/CB_RESEARCH/vocal_synthesis/"
-chunk_size = 200
+seq_lengths = [
+    100, 500, 1000, 2000, 4000, 8000
+]
+num_hidden_units = [
+    10, 20, 50, 100, 200
+]
 
-fs, data = wavfile.read(data_folder + os.path.sep + "data.wav")
-data = np.asarray(data, dtype="float32")
-#mean = np.mean(data)
-#std = np.std(data)
-#data = (data - mean) / std
-min_ = np.min(data)
-max_ = np.max(data)
-data = (data - min_) / (max_ - min_)
-X_total, y_total = generate_default(data, chunk_size+1)
+for seq_length in seq_lengths:
 
-valid_size = 0.1
-# ok, isolate out a validation set
-idxs = [x for x in range(0, X_total.shape[0])]
-train_idxs = idxs[ 0 : int(len(idxs)*(1-valid_size)) ]
-valid_idxs = idxs[ int(len(idxs)*(1-valid_size)) :: ]
-assert len(set(train_idxs).intersection(valid_idxs)) == 0
+    for num_hidden in num_hidden_units:       
 
-X_train, y_train = X_total[train_idxs], y_total[train_idxs]
-X_valid, y_valid = X_total[valid_idxs], y_total[valid_idxs]
+        with open(data_folder + os.path.sep + ("%i.pkl" % seq_length) ) as f:
+            dat = pickle.load(f)
+        X_train, X_valid, X_test = dat[0]
 
-sys.stderr.write("X_train shape = %s\n" % str(X_train.shape))
-sys.stderr.write("y_train shape = %s\n" % str(y_train.shape))
-sys.stderr.write("X_valid shape = %s\n" % str(X_valid.shape))
-sys.stderr.write("y_valid shape = %s\n" % str(y_valid.shape))
+        sys.stderr.write("X_train shape = %s\n" % str(X_train.shape))
+        sys.stderr.write("X_valid shape = %s\n" % str(X_valid.shape))
+        sys.stderr.write("X_test shape = %s\n" % str(X_test.shape))
 
-args = dict()
-args["batch_size"] = 128
-args["chunk_size"] = chunk_size
-args["learning_rate"] = 0.01
-args["momentum"] = 0.9
-args["num_epochs"] = 1000
-args["X_train"] = X_train
-args["y_train"] = y_train
-args["X_valid"] = X_valid
-args["y_valid"] = y_valid
+        args = dict()
+        args["num_inputs"] = 1
+        args["num_hidden_units"] = num_hidden
+        args["batch_size"] = 128
+        args["learning_rate"] = 0.01
+        args["momentum"] = 0.9
+        args["num_epochs"] = 1000
+        args["X_train"] = X_train
+        args["X_valid"] = X_valid
+        args["X_test"] = X_test
 
-model = experiment.train(args)
+        model = experiment.train(args)
 
-with open("out.model", "wb") as f:
-    #pickle.dump({"params": model, "mean": mean, "std": std}, f, pickle.HIGHEST_PROTOCOL)
-    pickle.dump({"params": model, "min": min_, "max": max_}, f, pickle.HIGHEST_PROTOCOL)
-
+        print "writing to file: s%i_h%i\n" % (seq_length, num_hidden)
+        with open("s%i_h%i" % (seq_length, num_hidden), "wb") as f:
+            pickle.dump(model, f, pickle.HIGHEST_PROTOCOL)
