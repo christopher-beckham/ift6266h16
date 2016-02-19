b34e7cbe86ec7ded274b4ebf315e30ae746b8b45
Christopher Beckham <chrispy645@gmail.com>
2016-02-19 11:17:53 -0500

    trying out new idea

---
+++ vocal_synthesis/19feb_testing.py
@@ -0,0 +1,40 @@
+import rnn_experiment as experiment
+import numpy as np
+from scipy.io import wavfile
+import os
+import sys
+import cPickle as pickle
+
+if __name__ == "__main__":
+
+    # e.g. 1000_60sec.pkl
+    in_pkl = sys.argv[1]
+    out_pkl = sys.argv[2]
+
+    with open(in_pkl) as f:
+        dat = pickle.load(f)
+    X_train, X_valid, X_test = dat[0]
+
+    sys.stderr.write("X_train shape = %s\n" % str(X_train.shape))
+    sys.stderr.write("X_valid shape = %s\n" % str(X_valid.shape))
+    sys.stderr.write("X_test shape = %s\n" % str(X_test.shape))
+
+    args = dict()
+    args["seed"] = 0
+    args["batch_size"] = 32
+    args["learning_rate"] = 0.01
+    args["momentum"] = 0.9
+    args["num_epochs"] = 100
+    args["X_train"] = X_train
+    args["X_valid"] = X_valid
+    args["X_test"] = X_test
+    
+    args["config"] = "19feb_testing.py"
+
+    model = experiment.train(args)
+
+    sys.stderr.write( "writing to file: %s\n" % (out_pkl) )
+    with open(out_pkl, "wb") as f:
+        pickle.dump(model, f, pickle.HIGHEST_PROTOCOL)
+
+
--- vocal_synthesis/create_pickles.sh
+++ vocal_synthesis/create_pickles.sh
@@ -6,7 +6,11 @@
 #done
 
 
-for seq in 10 50 100 500 1000 2000 4000 8000; do
-    echo "creating pickle ${seq}... for 60 seconds..."
-    python create_pickles.py ${seq} 60 /Volumes/CB_RESEARCH/vocal_synthesis/${seq}_60sec.pkl
-done
+#for seq in 10 50 100 500 1000 2000 4000 8000; do
+#    echo "creating pickle ${seq}... for 60 seconds..."
+#    python create_pickles.py ${seq} 60 /Volumes/CB_RESEARCH/vocal_synthesis/${seq}_60sec.pkl
+#done
+
+#python create_pickles_2.py 0.5 20 1800 $DATA_DIR/x0.5_20_1800.pkl
+
+python create_pickles_2.py 0.1 50 1800 $DATA_DIR/x0.1_50_1800.pkl
