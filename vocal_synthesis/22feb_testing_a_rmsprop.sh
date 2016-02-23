38115a94e0ac5ed011ec8f1a5a239f015a073fc2
Christopher Beckham <chrispy645@gmail.com>
2016-02-23 10:42:50 -0500

    add 22feb exps on 1 second chunks

---
+++ vocal_synthesis/22feb_testing_a_rmsprop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAME=22feb_testing_a_rmsprop
+
+THEANO_FLAGS=mode=FAST_RUN,device=gpu0,floatX=float32,nvcc.fastmath=True \
+	python -u $NAME.py $DATA_DIR/x1_50_all.pkl models/$NAME.model > models/$NAME.txt
---
+++ vocal_synthesis/22feb_testing_b_rmsprop.py
@@ -0,0 +1,42 @@
+import rnn_experiment as experiment
+import numpy as np
+from scipy.io import wavfile
+import os
+import sys
+import cPickle as pickle
+from lasagne.updates import *
+
+if __name__ == "__main__":
+
+    # e.g. 1000_60sec.pkl
+    in_pkl = sys.argv[1]
+    out_pkl = sys.argv[2]
+
+    with open(in_pkl) as f:
+        dat = pickle.load(f)
+    X_train, X_valid, X_test = dat[0]
+
+    sys.stderr.write("X_train shape = %s\n" % str(X_train.shape))
+    sys.stderr.write("X_valid shape = %s\n" % str(X_valid.shape))
+    sys.stderr.write("X_test shape = %s\n" % str(X_test.shape))
+
+    args = dict()
+    args["seed"] = 0
+    args["batch_size"] = 16
+    args["learning_rate"] = 0.01
+    args["momentum"] = 0.9
+    args["num_epochs"] = 1000
+    args["X_train"] = X_train
+    args["X_valid"] = X_valid
+    args["X_test"] = X_test
+    args["update_method"] = rmsprop
+    
+    args["config"] = "22feb_3x1000.py"
+
+    model = experiment.train(args)
+
+    sys.stderr.write( "writing to file: %s\n" % (out_pkl) )
+    with open(out_pkl, "wb") as f:
+        pickle.dump(model, f, pickle.HIGHEST_PROTOCOL)
+
+
