2963cea9629807dee5e601d5453b165c380ded3b
Christopher Beckham <chrispy645@gmail.com>
2016-02-25 22:05:50 -0500

    new configs and experiments

---
+++ vocal_synthesis/configurations/19feb_testing_f_relu.py
@@ -0,0 +1,38 @@
+import lasagne
+from lasagne.layers import *
+from lasagne.init import *
+from lasagne.nonlinearities import *
+from lasagne.objectives import *
+from lasagne.updates import *
+import sys
+
+def get_net(args):
+    # e.g. (bs, 50, 1800)
+    X_train = args["X_train"]
+    num_inputs = X_train.shape[2]
+    seq_length = X_train.shape[1] # determined by pkl
+
+    num_hidden_units = 200
+
+    if "seq_length" not in args:
+        seq_length = X_train.shape[1]
+    else:
+        seq_length = args["seq_length"]
+
+    grad_clip=10
+
+    l_input = InputLayer((None, seq_length, num_inputs))
+    l_forward = LSTMLayer(
+	l_input, num_units=num_hidden_units, unroll_scan=False, precompute_input=True, nonlinearity=rectify, grad_clipping=grad_clip, gradient_steps=10
+    )
+
+    """
+    In order to connect a recurrent layer to a dense layer, we need to
+    flatten the first two dimensions (our "sample dimensions"); this will
+    cause each time step of each sequence to be processed independently
+    """
+    l_shp = ReshapeLayer(l_forward, (-1, num_hidden_units))
+    l_dense = DenseLayer(l_shp, num_units=num_inputs, nonlinearity=rectify)
+    l_out = ReshapeLayer(l_dense, (-1, seq_length, num_inputs))
+    sys.stderr.write("Number of params in model: %i\n" % count_params(l_out))
+    return l_out
---
+++ vocal_synthesis/experiments/22feb_testing_b_rmsprop_rectify.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.io import wavfile
+import os
+import sys
+sys.path.append( os.pardir )
+import cPickle as pickle
+from lasagne.updates import *
+import rnn_experiment as experiment
+
+if __name__ == "__main__":
+
+    # e.g. 1000_60sec.pkl
+    in_pkl = sys.argv[1]
+    out_pkl = sys.argv[2]
+
+    with open(in_pkl) as f:
+        dat = pickle.load(f)
+    X_train, X_valid, X_test = dat[0]
+
+    sys.stderr.write("X_train shape = %s\n" % str(X_train.shape))
+    sys.stderr.write("X_valid shape = %s\n" % str(X_valid.shape))
+    sys.stderr.write("X_test shape = %s\n" % str(X_test.shape))
+
+    args = dict()
+    args["seed"] = 0
+    args["batch_size"] = 16
+    args["learning_rate"] = 0.01
+    args["momentum"] = 0.9
+    args["num_epochs"] = 5000
+    args["X_train"] = X_train
+    args["X_valid"] = X_valid
+    args["X_test"] = X_test
+    #args["update_method"] = nesterov_momentum
+    args["update_method"] = adagrad    
+
+    args["config"] = "../configurations/19feb_testing_e_relu.py"
+
+    model = experiment.train(args)
+
+    sys.stderr.write( "writing to file: %s\n" % (out_pkl) )
+    with open(out_pkl, "wb") as f:
+        pickle.dump(model, f, pickle.HIGHEST_PROTOCOL)
+
+
