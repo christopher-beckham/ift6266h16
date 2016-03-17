04ac2313c2420533303cd0dcc7b0b92099354e60
Christopher Beckham <chrispy645@gmail.com>
2016-03-17 09:33:43 -0400

    more exps

---
+++ vocal_synthesis/experiments/16mar_minimalist2_gauss2.py
@@ -0,0 +1,42 @@
+import numpy as np
+from scipy.io import wavfile
+import os
+import sys
+#os.chdir("..")
+sys.path.append( os.pardir )
+#sys.stderr.write("current working directory: %s\n" % os.getcwd())
+import cPickle as pickle
+from lasagne.updates import *
+import rnn_experiment as experiment
+
+if __name__ == "__main__":
+
+    # e.g. 1000_60sec.pkl
+    in_pkl = sys.argv[1]
+    out_pkl = sys.argv[2]
+
+    with open(in_pkl) as f:
+        dat = pickle.load(f)
+    X_train, X_valid, X_test = dat[0]
+
+    sys.stderr.write("X_train shape = %s\n" % str(X_train.shape))
+    sys.stderr.write("X_valid shape = %s\n" % str(X_valid.shape))
+    sys.stderr.write("X_test shape = %s\n" % str(X_test.shape))
+
+    args = dict()
+    args["seed"] = 0
+    args["batch_size"] = 16
+    args["learning_rate"] = 0.01
+    args["momentum"] = 0.9
+    args["num_epochs"] = 2000
+    args["X_train"] = X_train
+    args["X_valid"] = X_valid
+    args["X_test"] = X_test
+    args["update_method"] = rmsprop
+    args["out_pkl"] = out_pkl
+    args["sigma"] = 0.1 # level of gaussian noise
+    
+    args["config"] = "../configurations/19feb_testing_d_minimalist2_gauss.py"
+
+    experiment.train(args)
+
