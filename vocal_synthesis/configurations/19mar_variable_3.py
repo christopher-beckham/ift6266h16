b1b08eecc47bf012176a1f771b73c22946a683e4
Christopher Beckham <chrispy645@gmail.com>
2016-03-26 18:13:11 -0400

    try std normalisation and update config to specify forget gate bias

--- vocal_synthesis/configurations/19mar_variable_3.py
+++ vocal_synthesis/configurations/19mar_variable_3.py
@@ -34,11 +34,18 @@ def get_net(args):
 	    out_nonlinearity=tanh
 	    sys.stderr.write("using tanh out nonlinearity\n")
 
+    if "forget_gate" in args:
+        sys.stderr.write("using forget gate init with value = %i" % args["forget_gate"])
+        forget_gate = Gate(b=Constant(args["forget_gate"]))
+    else:
+        forget_gate = Gate(b=Constant(0.0))
+
     l_input = InputLayer((None, None, num_inputs))
     l_prev = l_input
     for unit in units:
     	l_forward = LSTMLayer(
-            l_prev, num_units=unit, unroll_scan=False, precompute_input=True, nonlinearity=nonlinearity
+            l_prev, num_units=unit, unroll_scan=False, precompute_input=True, nonlinearity=nonlinearity,
+            forgetgate=forget_gate
         )
         l_prev = l_forward
     l_shp = ReshapeLayer(l_forward, (-1, units[-1]))
