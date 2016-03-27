3a0ff259ff16e739e992b7e8d520545461d2e4cd
Christopher Beckham <chrispy645@gmail.com>
2016-03-27 16:05:35 -0400

    new round of exps

---
+++ vocal_synthesis/experiments/x0.5_5_all_offset10_std/19mar_2000x1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NAME=19mar_2000x1
+DATA=x0.5_5_all_offset10_std
+THEANO_FLAGS=mode=FAST_RUN,device=gpu0,floatX=float32,nvcc.fastmath=True \
+        python -u $NAME.py $DATA_DIR/$DATA.npy.npz \
+	../../models/$DATA/${NAME}.model > ../../models/$DATA/${NAME}.txt
