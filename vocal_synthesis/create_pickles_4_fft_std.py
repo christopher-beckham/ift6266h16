fbb86503a7e89cc589f28eeb1004e51b964b1310
Christopher Beckham <chrispy645@gmail.com>
2016-03-28 13:23:49 -0400

    forgot script

---
+++ vocal_synthesis/create_pickles_4_fft_std.py
@@ -0,0 +1,89 @@
+from scipy.io import wavfile
+import cPickle as pickle
+import sys
+import os
+import numpy as np
+import scipy
+
+def fft(x):
+    x_fft = scipy.fft(x)
+    return np.hstack( (x_fft.real, x_fft.imag) )
+
+test_start = 0.9
+valid_start = 0.8
+
+data_folder = os.environ["DATA_DIR"]
+
+fs, data = wavfile.read(data_folder + os.path.sep + "data.wav")
+data = np.asarray(data, dtype="float32")
+
+# if x_length == 0.5, then
+# x_length*fs = 16000*0.5 = 8000
+# so an x^i represents 0.5 of a second
+x_length = float(sys.argv[1])
+print "each x will represent %f samples (%f seconds)" % (x_length*fs, x_length)
+# a sequence is x_length*fs*seq_length long
+seq_length = int(sys.argv[2])
+print "each sequence will represent %f seconds" % (x_length*seq_length)
+
+how_many_seconds = int(sys.argv[3]) # 60*20 = 20 minutes
+
+offset_size = int(sys.argv[4])
+print "offset_size: %i" % offset_size
+
+
+if how_many_seconds > 0:
+    sys.stderr.write("truncating data to %i seconds\n" % how_many_seconds)
+    data = data[0: how_many_seconds*fs]
+
+# e.g. 0 -> 0.8
+train_data = data[ 0 : len(data)*valid_start ]
+# e.g. 0.8 -> 0.9 
+valid_data = data[ len(data)*valid_start : len(data)*test_start ]
+# e.g. 0.9 ::
+test_data = data[ len(data)*test_start :: ]
+
+dd = [train_data, valid_data, test_data]
+
+tmp_dirs = ["/tmp/dumps/train", "/tmp/dumps/valid", "/tmp/dumps/test"]
+for tmp in tmp_dirs:
+    os.makedirs(tmp)
+
+num_saved = 0
+for i in range(0, len(dd)):
+    x_size = int(x_length*fs)
+    batches = []
+
+    """
+    This is to generate 10x more training data, e.g
+    when offset=0, then we get things like
+    data[0:8000], data[8000:16000], etc. and
+    when offset=400 we get things like
+    data[400:8400], data[8400:16400], etc.
+    """
+    if offset_size > 0:
+        offsets = range(0, x_size, x_size/offset_size)
+    else:
+        offsets = [0]
+
+    for offset in offsets:
+        seq = []
+        b = 0
+        while True:
+            this_x = dd[i][ (b*x_size)+offset : ((b+1)*x_size)+offset ]
+            if len(this_x) != x_size:
+                break
+            seq.append( fft(this_x) / x_size )
+            if len(seq) == seq_length:
+                #batches.append(seq)
+                np.save( tmp_dirs[i] + ("/dump_%i.npy" % num_saved), seq )
+                num_saved += 1
+                seq = []
+            b += 1
+    #dd[i] = np.asarray(batches, dtype="float32")
+    #print "the shape of this array: %s" % (str(dd[i].shape))
+
+#with open(sys.argv[4], "wb") as f:
+#    pickle.dump( (dd, min_, max_), f, pickle.HIGHEST_PROTOCOL )
+
+print "saved %i npy files: " % num_saved
