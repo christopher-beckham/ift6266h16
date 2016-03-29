98dd403e37ec941c711f6f81c7f6391cbde99726
Christopher Beckham <chrispy645@gmail.com>
2016-03-28 21:38:18 -0400

    more exp

---
+++ vocal_synthesis/experiments/x0.25_10_all_offset5_fft_std/19mar_600x1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NAME=19mar_600x1
+DATA=x0.25_10_all_offset5_fft_std
+THEANO_FLAGS=mode=FAST_RUN,device=gpu0,floatX=float32,nvcc.fastmath=True \
+        python -u $NAME.py $DATA_DIR/$DATA.npy.npz \
+	../../models/$DATA/${NAME}.model > ../../models/$DATA/${NAME}.txt
